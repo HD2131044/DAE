90a5871e8b208a1f039c231ea88abeb0949f4a77
tiagoestacio <2130380@my.ipleiria.pt>
2015-11-11 16:49:26 +0000

    Adicionadas vistas e metodos relativos ao painel do Manager

--- src/java/dtos/CategoryDTO.java
+++ src/java/dtos/CategoryDTO.java
@@ -5,10 +5,37 @@
  */
 package dtos;
 
-/**
- *
- * @author ITWannaBe
- */
+
 public class CategoryDTO {
+    private Long id;
+    private String name;
     
+    public CategoryDTO(){
+        
+    }
+    
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;       
+    }
+    
+    public void reset(){
+        this.name = null;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
